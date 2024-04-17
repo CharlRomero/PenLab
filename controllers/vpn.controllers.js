39a9a2ecc5ad68ef74aaee0e27876a25d6f2a306
CharlRomero <romeokarlos12@gmail.com>
2024-04-17 14:06:36 -0500

    Refactor

--- controllers/vpn.controllers.js
+++ controllers/vpn.controllers.js
@@ -1,52 +1,21 @@
-import { Client } from "ssh2";
-import { HOST, PORT_SSH, USER, PATH_SSH } from "../config.js";
-import { readFileSync } from "fs";
+import { exec } from "child_process";
 
 export const createVpn = (req, res) => {
-  const username = req.body;
+  const { username } = req.body;
 
-  const conn = new Client();
-  const sshConfig = {
-    host: HOST,
-    port: PORT_SSH,
-    username: USER,
-    privateKey: readFileSync(PATH_SSH, "utf8"),
-  };
+  const command = `sh /home/penlab/generate_client.sh ${username}`;
 
-  conn
-    .on("ready", () => {
-      console.log("SSH Client :: ready");
-      conn.exec(
-        `sudo sh /home/penlab/generate_client.sh ${username}`,
-        (err, stream) => {
-          if (err) {
-            console.error(`Error al ejecutar el comando: ${err.message}`);
-            conn.end();
-            return res
-              .status(500)
-              .send("Error al ejecutar el comando en el servidor remoto");
-          }
-
-          let dataStream = "";
-          stream
-            .on("data", (data) => {
-              dataStream += data.toString();
-            })
-            .stderr.on("data", (data) => {
-              console.error("STDERR: " + data);
-            })
-            .on("close", () => {
-              console.log("Stream :: close");
-              conn.end();
-              res.send("Comando ejecutado exitosamente: " + dataStream);
-            });
-        }
-      );
-    })
-    .on("error", (err) => {
-      console.error("SSH Connection Error: ", err);
-      res.status(500).send("Error de conexión SSH");
-    });
-
-  conn.connect(sshConfig);
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return res
+        .status(500)
+        .send(`Error al ejecutar el script: ${error.message}`);
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return res.status(500).send(`Error en el script: ${stderr}`);
+    }
+    res.send(`Script ejecutado correctamente. ${stdout}`);
+  });
 };

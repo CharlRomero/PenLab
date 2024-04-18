7eedff56ee57bfaa88342440e6c0722588b249c4
CharlRomero <romeokarlos12@gmail.com>
2024-04-18 15:07:37 -0500

    Modify config

--- config.js
+++ config.js
@@ -7,6 +7,3 @@ export const PORT_DB = process.env.PORT_DB;
 export const USER_DB = process.env.USER_DB;
 export const PASSWORD_DB = process.env.PASSWORD_DB;
 export const DATABASE = process.env.DATABASE;
-export const PORT_SSH = process.env.PORT_SSH;
-export const USER = process.env.USER;
-export const PATH_SSH = process.env.PATH_SSH;

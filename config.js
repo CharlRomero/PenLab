310f583cadadd6d72a8e6ebec90164ae7a588a81
CharlRomero <romeokarlos12@gmail.com>
2024-04-19 11:22:40 -0500

    Added new configs

--- config.js
+++ config.js
@@ -3,9 +3,12 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const HOST = process.env.HOST;
-export const PORT_DB = process.env.PORT_DB;
-export const USER_DB = process.env.USER_DB;
-export const PASSWORD_DB = process.env.PASSWORD_DB;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DATABASE = process.env.DATABASE;
 export const COMMAND = process.env.COMMAND;
 export const PASSWORD = process.env.PASSWORD;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRE_TIME = process.env.JWT_EXPIRE_TIME;
+export const JWT_COOKIE_EXPIRES = process.env.JWT_COOKIE_EXPIRES;
--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 import { createPool } from "mysql2/promise";
-import { HOST, PORT_DB, USER_DB, PASSWORD_DB, DATABASE } from "./config.js";
+import { HOST, DB_PORT, DB_USER, DB_PASSWORD, DATABASE } from "./config.js";
 
 export const pool = new createPool({
   host: HOST,
-  port: PORT_DB,
-  user: USER_DB,
-  password: PASSWORD_DB,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
   database: DATABASE,
 });
 

75a2af92f93e8b03bd99c3f2741c629f7194878a
CharlRomero <romeokarlos12@gmail.com>
2024-04-19 10:54:30 -0500

    Added configs

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ import authRoutes from "./routes/auth.routes.js";
 import vpnRoutes from "./routes/vpn.routes.js";
 
 const app = express();
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(userRoutes);
 app.use(authRoutes);
 app.use(vpnRoutes);

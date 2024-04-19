6e3cb82e2d7140e5a47502046e9652a41cb8e074
CharlRomero <romeokarlos12@gmail.com>
2024-04-19 13:30:29 -0500

    Refactor auth

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -10,29 +10,25 @@ export const authentication = async (req, res) => {
       return res
         .status(400)
         .json({ message: "Username and password are required" });
-    pool.query(
+    const [result] = await pool.query(
       "SELECT * FROM user WHERE user_name = ?",
-      [username],
-      async (error, result) => {
-        if (
-          result.length == 0 ||
-          !(await bcryptjs.compare(password, result[0].user_password))
-        ) {
-          return res.status(401).json({ message: "Login failed" });
-        }
-        const id = result[0].id;
-        const token = jsonwebtoken.sign({ id: id }, JWT_SECRET, {
-          expiresIn: JWT_EXPIRE_TIME,
-        });
-        const cookiesOptions = {
-          expires: new Date(
-            Date.now() + JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-          ),
-        };
-        res.cookie("jwt", token, cookiesOptions);
-        res.json({ message: "Login successful", user: result[0] });
-      }
+      [username]
     );
+    if (
+      result.length == 0 ||
+      !(await bcryptjs.compare(password, result[0].user_password))
+    ) {
+      return res.status(401).json({ message: "Login failed" });
+    }
+    const id = result[0].id;
+    const token = jsonwebtoken.sign({ id: id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRE_TIME,
+    });
+    const cookiesOptions = {
+      expires: new Date(Date.now() + JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+    };
+    res.cookie("jwt", token, cookiesOptions);
+    res.json({ message: "Login successful", user: result[0] });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }

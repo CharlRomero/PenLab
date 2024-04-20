679608b62c56e559df7edad221e5a55a99fc9d75
CharlRomero <romeokarlos12@gmail.com>
2024-04-20 10:32:29 -0500

    Fix download

--- controllers/vpn.controllers.js
+++ controllers/vpn.controllers.js
@@ -24,7 +24,7 @@ export const createVpn = (req, res) => {
 };
 
 export const download_vpn = (req, res) => {
-  const { username } = req.body;
+  const { username } = req.params;
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const fileOvpn = path.join(
     __dirname,
